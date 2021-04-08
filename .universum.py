0be24a9d65bb500dfb949992b3749d5d03b95a50
Kateryna Dovgan <46348880+k-dovgan@users.noreply.github.com>
2021-04-08 17:38:15 +0300

    Update .universum.py

--- .universum.py
+++ .universum.py
@@ -3,9 +3,9 @@
 from universum.configuration_support import Configuration, Step
 
 configs = Configuration([
-    Step(name='Run script', command=['python', 'run.py', 'pass']),
+    Step(name='Run script', command=['python3.7', 'run.py', 'pass']),
     dict(name="Pylint check", code_report=True, command=[
-        "python", "-m", "universum.analyzers.pylint", "--python-version", "3.7",
+        "python3.7", "-m", "universum.analyzers.pylint", "--python-version", "3.7",
         "--result-file", "${CODE_REPORT_FILE}", "--files", "*.py"
     ])
 ])
