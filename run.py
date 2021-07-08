8da8a7cfa3185c64919aaf3035df70546bada86b
Kateryna Dovgan <46348880+k-dovgan@users.noreply.github.com>
2021-07-08 19:27:52 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
+
 #!/usr/bin/env python
 
 import sys
+
+with open("execution.log", "w+") as new_file:
+    new_file.write("Here's what a script accepted from command line:\n" + str(sys.argv))
 
 if len(sys.argv) < 2:
     print("Unknown outcome")
